9b1f16f4e28d5a36debb1c4d77dab02b51d56de8
Whekin <stanislavkalishin@gmail.com>
2019-03-26 20:33:33 +0300

    Add leaflet map on HomePage

--- imports/ui/components/HomePage.jsx
+++ imports/ui/components/HomePage.jsx
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import 'leaflet/dist/leaflet.css';
+import { Map as LeafletMap, Marker, Popup, TileLayer } from 'react-leaflet';
 
 class HomePage extends Component {
+  state = {
+    lat: 47.227088,
+    lng: 39.714387,
+    zoom: 13
+  }
+
   render() {
+    const position = [this.state.lat, this.state.lng];
+
     return (
-      <div className="HomePage">
-        HomePage
-      </div>
+      <LeafletMap
+        center={position}
+        zoom={this.state.zoom}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position} >
+          <Popup>
+            Milk&Cartoons
+          </Popup>
+        </Marker>
+      </LeafletMap>
     );
   }
 }
