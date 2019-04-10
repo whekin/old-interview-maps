52269384645580353344a656babaeda90e94c70c
Whekin <stanislavkalishin@gmail.com>
2019-04-10 15:34:23 +0300

    Add Loading page

--- imports/ui/components/App.jsx
+++ imports/ui/components/App.jsx
@@ -4,11 +4,11 @@ import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Provider } from 'react-redux';
 import Notifications from './Notifications';
+import store from '../redux';
 import '/imports/ui/styles/main.scss';
 import StartPage from './StartPage';
 import HomePage from './HomePage';
-
-import store from '../redux';
+import Loading from './Loading';
 
 class App extends Component {
   render() {
@@ -19,7 +19,11 @@ class App extends Component {
         <Router>
           <div className="App">
             <Route exact path="/" render={
-              () => !currentUser ? <Redirect to="/signup" /> : ""} />
+              () => currentUser === null
+                ? <Redirect to="/signup" />
+                : currentUser === undefined
+                  ? <Loading /> : ""
+            } />
             <Route path="/(signup|login)" render={
               () => currentUser ? <Redirect to="/" /> : ""} />
 
@@ -36,5 +40,7 @@ class App extends Component {
 }
 
 export default withTracker(() => ({
+  // if it returns undefined it means that it's loading. And if
+  // it returns null then it means that user is not authorized.
   currentUser: Meteor.user()
 }))(App);
